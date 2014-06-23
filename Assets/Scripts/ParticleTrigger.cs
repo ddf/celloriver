4aedf49ebadc55f63fe9a7e2bb96d9e38f86576d
Damien Di Fede <dcomposer@gmail.com>
2014-06-23 17:18:26 -0400

    updated project, added midi plugin, added max patch to test with midi plugin

---
+++ Assets/Scripts/ParticleTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    void OnNoteOn (MidiMessage midi)
+    {
+        particleSystem.startSize = midi.data2 / 64.0f;
+        particleSystem.Emit (Mathf.Max(midi.data2 / 8, 3));
+    }
+}
