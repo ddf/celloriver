f5cdf1e42b15a440dda99028ec1e564cf8d3415d
Damien Di Fede <dcomposer@gmail.com>
2014-08-12 13:46:06 -0500

    added time of day dome

---
+++ Assets/Time of Day/Demo/Scripts/MouseLook.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float paddingLeft   = 0;
+    public float paddingRight  = 0;
+    public float paddingTop    = 0;
+    public float paddingBottom = 0;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    void Update()
+    {
+        if (!Input.GetKey("mouse 0") ||
+            Input.mousePosition.x < paddingLeft || Input.mousePosition.x > Screen.width - paddingRight ||
+            Input.mousePosition.y < paddingTop  || Input.mousePosition.y > Screen.width - paddingBottom)
+        {
+            return;
+        }
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+    void Start()
+    {
+        if (rigidbody) rigidbody.freezeRotation = true;
+    }
+}
